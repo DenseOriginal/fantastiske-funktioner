5de351b4600c8459e17556d6da473b8a70724e5c
Anders Kok <48748922+DenseOriginal@users.noreply.github.com>
2022-03-21 21:29:00 +0100

    Added header

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-import Grapher from './lib/Grapher.svelte'
 </script>
 
+<header>
+  <h1>Fantastiske Funktioner</h1>
+</header>
 <main>
-  <Grapher width={400} height={400} equation={(x) => 2**x} />
+  <!-- <Grapher width={400} height={400} equation={(x) => 2**x} /> -->
 </main>
 
 <style>
